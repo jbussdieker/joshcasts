ba8367bf3afe8b96ae4de168d7ad2a6c8115101d
Joshua Bussdieker <jbussdieker@gmail.com>
2013-02-27 19:17:33 -0800

    New app

---
+++ app/controllers/tutorials_controller.rb
@@ -0,0 +1,43 @@
+class TutorialsController < ApplicationController
+  def index
+    @tutorials = Tutorial.all
+  end
+
+  def show
+    @tutorial = Tutorial.find(params[:id])
+  end
+
+  def new
+    @tutorial = Tutorial.new
+  end
+
+  def edit
+    @tutorial = Tutorial.find(params[:id])
+  end
+
+  def create
+    @tutorial = Tutorial.new(params[:tutorial])
+    if @tutorial.save
+      redirect_to @tutorial, notice: 'Tutorial was successfully created.'
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @tutorial = Tutorial.find(params[:id])
+
+    if @tutorial.update_attributes(params[:tutorial])
+      redirect_to @tutorial, notice: 'Tutorial was successfully updated.'
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @tutorial = Tutorial.find(params[:id])
+    @tutorial.destroy
+
+    redirect_to tutorials_url
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joshcasts::Application.config.secret_token = '525abf8b55f29261d62c574712ae344622dd71c54ae3a390b339eaed389b78248fc599356e2fbce4f38ed84b9676346df4376d4b7366af75b6199990dc183073'
